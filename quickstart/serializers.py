7a46dbfedf3dc55d4b8004484f0df8a16d6a816c
Roshan Pandey <pandeyroshan556@gmail.com>
2019-08-20 21:31:36 +0530

    Update serializers.py

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import DataSet
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class DataSetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DataSet
+        fields = ['data']
