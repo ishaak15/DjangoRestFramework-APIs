827ef8d0af087e71e95c8f32106f9d162e65866b
Roshan Pandey <pandeyroshan556@gmail.com>
2019-08-20 21:33:20 +0530

    Update models.py

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DataSet(models.Model):
+    data = models.CharField(max_length=20,blank=False)
+
+    def __str__(self):
+        return self.data
